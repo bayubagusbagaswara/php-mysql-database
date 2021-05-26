6ee24a8569b16b1424b590ea6c88f9d0493ccc93
bayubagusbagaswara <bagaszwara12@gmail.com>
2021-05-26 10:58:44 +0700

    buat file getConnection

---
+++ GetConnection.php
@@ -0,0 +1,12 @@
+<?php
+// file ini berisi function khusus untuk membuat object connection PDO
+function getConnection(): PDO
+{
+    $host = "localhost";
+    $port = 3306;
+    $database = "belajar_php_database";
+    $username = "root";
+    $password = "";
+
+    return new PDO("mysql:host=$host:$port;dbname=$database", $username, $password);
+}
